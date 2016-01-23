781386800371431a9e378341c7ecff71f300faed
Simon Vandel Sillesen <simon.vandel@gmail.com>
2016-01-23 18:23:47 +0100

    macair: touchpad, tlp, upower
    * use mtrack instead of synaptics
    * power savings by using tlp and upower

--- vandel-macair-conf.nix
+++ vandel-macair-conf.nix
@@ -24,12 +24,53 @@
 
   networking.hostName = "vandel-macair";
 
-  # touchpad
-  services.xserver.synaptics = {
-    enable = true;
-    tapButtons = true;
-    fingersMap = [ 0 0 0 ];
-    buttonsMap = [ 1 3 2 ];
-    twoFingerScroll = true; 
+  services = {
+    #mba6x_bl.enable = true; # gives black screen on boot right now
+
+    # power savings
+    upower.enable = true;
+    tlp.enable = true;
+
+    # touchpad
+    xserver.multitouch = {
+      enable = true;
+      buttonsMap = [ 1 3 2 ];
+      additionalOptions = ''
+        Option "Sensitivity" "1"
+        Option "FingerHigh" "5"
+        Option "FingerLow" "4"
+        Option "IgnoreThumb" "true"
+        Option "IgnorePalm" "true"
+        Option "TapButton1" "1"
+        Option "TapButton2" "3"
+        Option "TapButton3" "0"
+        Option "TapButton4" "0"
+        Option "ClickFinger1" "1"
+        Option "ClickFinger2" "3"
+        Option "ClickFinger3" "3"
+        Option "ButtonMoveEmulate" "true"
+        Option "ButtonIntegrated" "true"
+        Option "ClickTime" "25"
+        Option "BottomEdge" "10"
+        Option "SwipeLeftButton" "8"
+        Option "SwipeRightButton" "9"
+        Option "SwipeUpButton" "0"
+        Option "SwipeDownButton" "0"
+        Option "ScrollDistance" "75"
+        Option "ScrollUpButton" "4"
+        Option "ScrollDownButton" "5"
+        Option "ThumbSize" "35"
+        Option "PalmSize" "55"
+        Option "DisableOnThumb" "false"
+        Option "DisableOnPalm" "true"
+        Option "SwipeDistance" "1000"
+        Option "ScrollLeftButton" "7"
+        Option "ScrollRightButton" "6"
+        Option "AccelerationProfile" "1"
+        Option "ConstantDeceleration" "2.0" # Decelerate endspeed
+        Option "AdaptiveDeceleration" "2.0" # Decelerate slow movements 
+        Option "ScrollSmooth" "true" # Experiment
+      '';
+    };
   };
 }
