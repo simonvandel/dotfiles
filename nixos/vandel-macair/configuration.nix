5dd40004718f6a68a08fd0406ce66bb392ca4777
Simon Vandel Sillesen <simon.vandel@gmail.com>
2016-03-05 11:17:40 +0100

    macair: use synaptics instead of multitrack for touchpad. Gives smooth scroll in supported toolkits e.g. GTK3

--- nixos/vandel-macair/configuration.nix
+++ nixos/vandel-macair/configuration.nix
@@ -42,45 +42,12 @@
       xkbOptions = "ctrl:nocaps"; # make capslock = ctrl
 
       # touchpad
-      multitouch = {
+      synaptics = {
         enable = true;
-        buttonsMap = [ 1 3 2 ];
-        additionalOptions = ''
-          Option "Sensitivity" "1"
-          Option "FingerHigh" "5"
-          Option "FingerLow" "4"
-          Option "IgnoreThumb" "true"
-          Option "IgnorePalm" "true"
-          Option "TapButton1" "1"
-          Option "TapButton2" "3"
-          Option "TapButton3" "0"
-          Option "TapButton4" "0"
-          Option "ClickFinger1" "1"
-          Option "ClickFinger2" "3"
-          Option "ClickFinger3" "3"
-          Option "ButtonMoveEmulate" "true"
-          Option "ButtonIntegrated" "true"
-          Option "ClickTime" "25"
-          Option "BottomEdge" "10"
-          Option "SwipeLeftButton" "8"
-          Option "SwipeRightButton" "9"
-          Option "SwipeUpButton" "0"
-          Option "SwipeDownButton" "0"
-          Option "ScrollDistance" "75"
-          Option "ScrollUpButton" "4"
-          Option "ScrollDownButton" "5"
-          Option "ThumbSize" "35"
-          Option "PalmSize" "55"
-          Option "DisableOnThumb" "false"
-          Option "DisableOnPalm" "true"
-          Option "SwipeDistance" "1000"
-          Option "ScrollLeftButton" "7"
-          Option "ScrollRightButton" "6"
-          Option "AccelerationProfile" "1"
-          Option "ConstantDeceleration" "2.0" # Decelerate endspeed
-          Option "AdaptiveDeceleration" "2.0" # Decelerate slow movements
-          Option "ScrollSmooth" "true" # Experiment
-        '';
+        accelFactor = "0.1";
+        minSpeed = "0.5";
+        maxSpeed = "1";
+        tapButtons = true;
       };
     };
   };
